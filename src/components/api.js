6696f7ab5c43e0b6e37a21cd5b7a993ec9760f5c
Shrikant Ramani <shrikantramani@Shrikants-MacBook-Air.local>
2022-08-03 19:19:38 +0530

    first commit

---
+++ src/components/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET', 
+	headers: {
+		'X-RapidAPI-Key': '0ef7b32dc4msh670ba06319f74cep1aeb0djsnfce4ee9cacb9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "5bb0ec6795041f124952ba2ea8b14981"
